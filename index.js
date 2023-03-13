7fd50f347dcf4eeea1256f64e0e38eb02f0e16b8
Tofu <sofia.sarasty.12@gmail.com>
2023-03-13 07:54:44 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var dos = "Dar comida a Otis"
+var uno = "Limpiar arena de Otis"
+var tres = "Ordenar cuarto"
+var cuatro = "Hacer brochure"
+var cinco = "Lavar ropa"
+var seis = "Comprar comida"
+var siete = "Limpiar espejos"
+var ocho = "Ver película con mi hermana"
+var nueve = "Quejarme de la vida"
+var diez = "Quejarme de la gente"
+
+const contenerdorRef = document.getElementById("aúnnosé")
+for (let i = 0; i < 10; i++) {
+    const lista =
+    `<button>${i}</button> `
+        aúnnosé.innerhtml += lista
+    }
